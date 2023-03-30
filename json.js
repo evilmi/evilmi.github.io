d1f8747153c6759a09409dbf7c8923e877c978c0
evilmi <128274631+evilmi@users.noreply.github.com>
2023-03-30 21:33:41 +0800

    Add files via upload

---
+++ json.js
@@ -0,0 +1,13 @@
+/**
+ * 配置文件：
+ * 1、配置背景图片与视频；
+ * 2、配置轮播广告图片与链接；
+ * 3、配置轮播新闻的内容与链接；
+ * **/
+var GLOBAL = {
+  bg:{
+    mainBG:'video',           //背景显示的是视频还是图片;video表示只显示本地视频，image表示只显示背景图片
+    video:'./video_bg/video.mp4',   //相对html的视频文件路径
+    image:'./video_bg/bg.jpg'       //相对html的背景图片路径
+  },
+}
